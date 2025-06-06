c990491e3b11c554bafed2fa9993d86bc4b9f5eb
Andi Northrop <andi@northrop.uk>
2025-06-06 06:16:05 +0100

    Re-uploading, respecting the new .gitignore

---
+++ functions.py
@@ -0,0 +1,42 @@
+import os, random
+import tvdb_v4_official
+import json
+
+tvdb_api = os.getenv("TVDB_API_KEY")
+tvdb = tvdb_v4_official.TVDB(tvdb_api)
+
+def tvdb_search(search_term):
+    results = tvdb.search(search_term)
+    shows = []
+    for result in results:
+        try:
+            show_synopsis = result["overview"]
+        except KeyError:
+            show_synopsis = "no synopsis"
+        show = {
+            "show_name": result["name"],
+            "show_id": result["tvdb_id"],
+            "show_synopsis": show_synopsis,
+        }
+        shows.append(show)
+    return shows
+
+def tvdb_episodes(show_id):
+    show = tvdb.get_series_extended(show_id)
+    season_ids = []
+    for season in show["seasons"]:
+        if season["type"]["id"] == 1 and season["number"] > 0:
+            season_ids.append(season["id"])
+    rand_season_id = season_ids[random.randrange(0, len(season_ids))]
+    rand_season_info = tvdb.get_season_extended(rand_season_id)
+    rand_episode_index = random.randrange(0, len(rand_season_info["episodes"]))
+    rand_episode = rand_season_info["episodes"][rand_episode_index]
+
+    return show, rand_episode
+
+if __name__ == "__main__":
+    # print(tvdb.search("The West Wing"))
+    show, episode = tvdb_episodes(259972)
+    print(f"Show: {show["name"]}\nSeason: {episode["seasonNumber"]}\n"
+          f"Episode: {episode["number"]}\nEpisode Title: {episode["name"]}\n"
+          f"Episode synopsis: {episode["overview"]}")
---
+++ random_episode/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.HomePageView.as_view(), name="home"),
+    path("search/", views.search_results, name="search_results"),
+    path("episode/", views.random_episode, name="random_episode"),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path("contact/", views.ContactView.as_view(), name="contact")
+]
