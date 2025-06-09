4c991fc6eee6d26d4fb8fc55b37aff9d0d8e8c94
Andi Northrop <andi@northrop.uk>
2025-06-09 06:36:03 +0100

    Removes show ids from search results and allows local server again.

--- randomepisode_project/settings.py
+++ randomepisode_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['randomepisode.northrop.uk']
+ALLOWED_HOSTS = ['randomepisode.northrop.uk', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
